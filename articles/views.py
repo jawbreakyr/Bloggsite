8e38654a14de6f96024cc56cc1eded72459a5f04
hubert <jawbreakyr@gmail.com>
2014-07-25 11:25:15 +0800

    understanding views

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from articles.models import Category, Article
 
 def home(request):
-	return render(request, "home.html")
+	trash_metal_article = Article.objects.filter(category__slug='trash-metal').order_by('-published_date')[:]
+	grind_core_article = Article.objects.filter(category__slug='grind-core').order_by('-published_date')[:]
+	death_metal_article = Article.objects.filter(category__slug='death-metal').order_by('-published_date')[:]
+
+
+	return render(request, "home.html", {
+		'death_metal_article': death_metal_article,
+		'grind_core_article': grind_core_article,
+		'trash_metal_article': trash_metal_article,
+		})
 # Create your views here.
